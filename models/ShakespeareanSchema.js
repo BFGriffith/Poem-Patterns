e08094e628122dcbc0fa1642dfdab24f74d4265a
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-09-12 19:28:02 -0400

    working on Mongoose schema

---
+++ models/ShakespeareanSchema.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ShakespeareanSchema = new Schema({
+	poets: [{
+		type: Schema.Types.ObjectId,
+		ref: 'Poet'
+	}],
+	ShakespeareanSonnets: [{
+    ShakespeareanSonnet: [{
+      limerickStanza1: [{
+        limerickLine1: String,
+        limerickLine2: String,
+        limerickLine3: String,
+        limerickLine4: String,
+        limerickLine5: String,
+      }]
+    }]
+  }],
+  annotations: [{
+    poemAnnotatedTitle: String,
+    annotation: String,
+    user: Object
+  }]
+});
+
+module.exports = mongoose.model('Shakespearean', ShakespeareanSchema);
---
+++ models/haikuSchema.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var HaikuSchema = new Schema({
+	poets: [{
+		type: Schema.Types.ObjectId,
+		ref: 'Poet'
+	}],
+	haikuPoems: [{
+    haiku: [
+      haikuLine1: String,
+      haikuLine2: String,
+      haikuLine3: String
+    ]
+  }],
+  annotations: [{
+    poemAnnotatedTitle: String,
+    annotation: String,
+    user: Object
+  }]
+});
+
+module.exports = mongoose.model('Haiku', HaikuSchema);
