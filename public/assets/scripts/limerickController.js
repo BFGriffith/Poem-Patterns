0d8c6feac08e133e65fd852e9bc6c2663e1b6177
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-09-17 05:39:50 -0400

    still cannot figure out how to chain AJAX to last-word capture with jQuery

--- public/assets/scripts/limerickController.js
+++ public/assets/scripts/limerickController.js
@@ -28,20 +28,27 @@ $(document).ready(function() {
         var last_arr = $('#testing').text().split(' ');
         var las = last_arr[last_arr.length - 1];
         console.log(las);
+        return las;
       }
 
-      // setTimeout(function() {
-      //   $.ajax({
-      //       method: "GET",
-      //       url: "http://rhymebrain.com/talk?function=getRhymes&word=" + rhymeQuery,
-      //       // data: 
-      //     })
-      //     .done(function(data) {
-      //       console.log("Data Saved: " + data);
-      //       var rhymeAPIresponse = JSON.parse(data);
-      //       console.log(rhymeAPIresponse);
-      //     });
-      // }, 1000);
+
+      setTimeout(function(las) {
+        var rhymeQueryString = "http://rhymebrain.com/talk?function=getRhymes&word=" + las + "&lang=en&jsonp=";
+
+        $.ajax({
+          method: "GET",
+          url: rhymeQueryString
+        })
+        // $.ajax(settings).done(function(response) {
+        //   console.log(response);
+        // });
+        .done(function(data) {
+          console.log("Data Saved: " + data);
+          //var rhymeAPIresponse = JSON.parse(data);
+          //console.log(rhymeAPIresponse);
+        });
+      }, 1000);
+
     } else {
       console.log("checkbox deselected");
     }
